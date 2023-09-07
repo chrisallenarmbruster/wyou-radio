e070df4d43850218a9a7423a8b56f1045d3ab5fb
joel <jejanov@gmail.com>
2023-09-06 20:43:18 -0600

    cleanup

--- client/store/playlistSlice.js
+++ client/store/playlistSlice.js
@@ -14,24 +14,20 @@ const playlistSlice = createSlice({
   reducers: {
     addTrack: (state, action) => {
       state.tracks.push(action.payload);
-      // exportPlaylist(state.tracks);
     },
     addTracks: (state, action) => {
       state.tracks = [...state.tracks, ...action.payload];
-      // exportPlaylist(state.tracks);
     },
     removeTrack: (state, action) => {
       state.tracks = state.tracks.filter(
         (track) => track.id !== action.payload.id
       );
-      // exportPlaylist(state.tracks);
     },
     clearPlaylist: (state) => {
       state.tracks = [];
     },
     setPlaylistLoading: (state, action) => {
       state.loading = action.payload;
-      // exportPlaylist(state.tracks);
     },
     setPlaylistError: (state, action) => {
       state.error = action.payload;
@@ -84,28 +80,6 @@ export const fetchPlaylistTracks =
       dispatch(setPlaylistLoading(false));
     }
   };
-
-export const exportPlaylist = async (playlist) => {
-  try {
-    const response = await axios.post("api/content/add-playlist", {
-      playlist: playlist,
-    });
-    console.log(response.data);
-    debugger;
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-export const nextDJTrack = async (index) => {
-  try {
-    const response = await axios.get(`api/content/next-content?index=${index}`);
-    console.log(response.data);
-    debugger;
-  } catch (error) {
-    console.log(error);
-  }
-};
 
 export const fetchQueueTracks = () => async (dispatch, getState) => {
   try {
