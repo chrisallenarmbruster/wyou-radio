f7c288ed407f03261c07cebe492263133546700e
joel <jejanov@gmail.com>
2023-09-18 16:02:32 -0600

    styling

--- client/Components/DiscJockeys.js
+++ client/Components/DiscJockeys.js
@@ -1,144 +1,144 @@
-import React, { useRef, useState, useEffect } from "react"
-import { connect } from "react-redux"
-import { setCurrentDj } from "../store/djsSlice"
-import { Swiper, SwiperSlide } from "swiper/react"
-import Container from "react-bootstrap/Container"
-import Col from "react-bootstrap/Col"
-import Row from "react-bootstrap/Row"
-import Image from "react-bootstrap/Image"
-import Button from "react-bootstrap/Button"
-import { useNavigate } from "react-router-dom"
+import React, { useRef, useEffect, useState } from "react";
+import { connect } from "react-redux";
+import { setCurrentDj } from "../store/djsSlice";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Col, Row, Image, Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import { FaMicrophone } from "react-icons/fa";
+import { MdSmartButton } from "react-icons/md";
 
-import "swiper/css"
-import "swiper/css/effect-fade"
-import "swiper/css/navigation"
-import "swiper/css/pagination"
+import "swiper/css";
+import "swiper/css/effect-fade";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
-import "./djsStyle.css"
+import "./djsStyle.css";
 
-import { EffectFade, Navigation, Pagination } from "swiper/modules"
+import { EffectFade, Navigation } from "swiper/modules";
 
-let djAudioGreeting = new Audio()
+let djAudioGreeting = new Audio();
 
 export function DiscJockeys(props) {
   useEffect(() => {
     return () => {
-      djAudioGreeting.pause()
-    }
-  }, [])
+      djAudioGreeting.pause();
+    };
+  }, []);
 
-  useEffect(() => {
-    setTimeout(() => {
-      // sliderRef.current.swiper.slideTo(0)
-    }, 100)
-  }, [])
+  const navigate = useNavigate();
+  const sliderRef = useRef();
 
-  const navigate = useNavigate()
-
-  const sliderRef = useRef()
+  const [isPlaying, setIsPlaying] = useState(false);
 
   function handleDjAudioGreeting(dj) {
-    console.log("dj", dj)
-    djAudioGreeting.src =
-      dj.details?.audio ||
-      "audio/ElevenLabs_2023-09-01T23_59_37_Donny - very deep_gen_s50_sb75_se0_b_m2.mp3"
-    djAudioGreeting.play()
+    if (
+      djAudioGreeting.src !==
+      (dj.details?.audio ||
+        window.location.origin + "/audio/default_audio_path.mp3")
+    ) {
+      djAudioGreeting.src =
+        dj.details?.audio || "/audio/default_audio_path.mp3";
+    }
+
+    djAudioGreeting.onplay = () => setIsPlaying(true);
+    djAudioGreeting.onended = () => setIsPlaying(false);
+    djAudioGreeting.onpause = () => setIsPlaying("paused");
+
+    if (djAudioGreeting.paused) {
+      if (isPlaying === "paused") {
+        djAudioGreeting.play();
+      } else {
+        djAudioGreeting.currentTime = 0;
+        djAudioGreeting.play();
+      }
+    } else {
+      djAudioGreeting.pause();
+    }
   }
 
   function handleSelectDj(dj) {
-    props.selectDj(dj)
-    if (props.currentStation) {
-      navigate(`/radio/player`)
-    } else {
-      navigate(`/radio/stations`)
-    }
+    props.selectDj(dj);
+    navigate(props.currentStation ? `/radio/player` : `/radio/stations`);
   }
 
   return (
     <>
       <div className="text-light">
-        <h1 className="h3 mt-3">Select Disc Jockey</h1>
+        <h1 className="h4 mt-3">Select Your Disc Jockey</h1>
       </div>
       <Swiper
         ref={sliderRef}
-        spaceBetween={30}
-        autoHeight={true}
+        spaceBetween={20}
         slidesPerView={1}
-        // initialSlide={0}
-        effect={"fade"}
-        // grabCursor={true}
-        centeredSlides={true}
-        navigation={true}
-        loop={true}
+        effect="fade"
+        centeredSlides
+        navigation
+        loop
         modules={[EffectFade, Navigation]}
-        className="mySwiper "
+        className="mySwiper"
         onSlideChange={() => {
-          djAudioGreeting.pause()
+          djAudioGreeting.pause();
         }}
       >
         {props.djs.map((dj, idx) => (
-          <SwiperSlide
-            key={`dj-${idx}`}
-            className="bg-dark text-light "
-            style={{ width: "100%", height: "100%" }}
-          >
-            <div
-              className="bg-dark "
-              style={{
-                height: "100%",
-                width: "100%",
-                overflowX: "hidden",
-                overflowY: "auto",
-              }}
-            >
-              <Row className=" bg-dark text-light">
-                <Col sm={12} md={6} className="px-5">
-                  <Image src={dj.details?.image} />
-                </Col>
-                <Col
-                  sm={12}
-                  md={6}
-                  className="px-5 text-start bg-dark mt-3 mt-md-0"
-                >
-                  <h2 className="h3">
-                    {dj.djName}
-                    <Button
-                      className="ms-3"
-                      size="sm"
+          <SwiperSlide key={`dj-${idx}`} className="bg-dark text-light">
+            <Row className="bg-dark text-light p-3">
+              <Col sm={12} md={6} className="pl-5">
+                <Image
+                  src={dj.details?.image}
+                  fluid
+                  className="rounded-image"
+                />
+              </Col>
+              <Col
+                sm={12}
+                md={6}
+                className="pr-5 text-start bg-dark mt-2 mt-md-0"
+              >
+                <h2 className="h5 dj-name-container">
+                  <span className="dj-name">{dj.djName}</span>
+                  <div className="buttons-container">
+                    <FaMicrophone
+                      size={15}
+                      className={`microphone-icon ${
+                        isPlaying === true
+                          ? "playing"
+                          : isPlaying === "paused"
+                          ? "paused"
+                          : ""
+                      }`}
                       onClick={() => handleDjAudioGreeting(dj)}
-                    >
-                      Greeting
-                    </Button>
-                    <Button
-                      className="mx-3"
-                      size="sm"
+                    />
+                    <button
+                      className="select-button"
                       onClick={() => handleSelectDj(dj)}
                     >
                       Select
-                    </Button>
-                  </h2>
-                  <p>{dj.details?.context}</p>
-                </Col>
-              </Row>
-            </div>
+                    </button>
+                  </div>
+                </h2>
+
+                <p style={{ textAlign: "justify" }}>{dj.details?.context}</p>
+              </Col>
+            </Row>
           </SwiperSlide>
         ))}
       </Swiper>
     </>
-  )
+  );
 }
 
 const mapStateToProps = (state) => {
   return {
     djs: state.djs.allDjs,
     currentStation: state.stations.currentStation,
-  }
-}
+  };
+};
 
 const mapDispatchToProps = (dispatch) => {
   return {
     setCurrentDj: (dj) => dispatch(setCurrentDj(dj)),
-  }
-}
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(DiscJockeys)
+export default connect(mapStateToProps, mapDispatchToProps)(DiscJockeys);
